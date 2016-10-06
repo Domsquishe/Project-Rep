6efd5ce3b963f7416ad6112704ff6be29d36820e
Domsquishe <sulstond@gmail.com>
2016-10-06 10:22:45 +0100

    Update project_display.php

--- project_display.php
+++ project_display.php
@@ -1,6 +1,4 @@
-
 <html>
-
 <head>
     <title> Meetup Project </title>
     <style>
@@ -26,7 +24,7 @@
     </style>
 
 </head>
-
+ 
 <body>
 
 <div class = "title">
@@ -40,67 +38,109 @@
     <h4 id="geocode"></h4>
 </div>    
     
-    
-    <?php 
-     
-        $address1 = $_POST['address1'];
-        $address2 = $_POST['address2'];
-    ?>
-    
-    <script>
-    
-    var var_lat;
-    var var_long;
-    
-    var xmlhttp = new XMLHttpRequest();
-    var entered_location = "<?php echo $address; ?>";
-    var url = "https://maps.googleapis.com/maps/api/geocode/json?address=".$address1."&key=AIzaSyBN5Q_GVvi5ONp6lwgmIlWG72NKtZUB9pU";
-    xmlhttp.onreadystatechange = function() {
-        if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
-            var myArr = JSON.parse(xmlhttp.responseText);
-            FormatAddress(myArr.results[0]);
-        }
-    };
-    xmlhttp.open("GET", url, true);
-    xmlhttp.send();
-    
-    function FormatAddress(arr) 
-    {
-        var var_address = "";
-        var_address += arr.formatted_address;
-        var_lat = parseFloat((arr.geometry.location.lat));
-        var_long = parseFloat((arr.geometry.location.lng));
-        
-        console.log(var_lat, var_long);
-        
-        
-        document.getElementById("geocode").innerHTML = var_address;
-        $("#geocode").text(out);
-        return var_lat;
-        return var_long;
-    }
-</script>
-    
-    
-    <!-- create map -->
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.0.0/jquery.js"></script>
-    <script>
-          var map;
-          function initMap() 
-            {
-                map = new google.maps.Map(document.getElementById('map'),
-                {center: {lat: 52.2831, lng: -1.5621},zoom: 5});
-                 console.log(var_lat, var_long);
-                var myLatLng = {lat: var_lat, lng: var_long};
-                var marker = new google.maps.Marker({position: myLatLng, map: map, title: 'Address 1'});
-            }
-        </script>
-        <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBN5Q_GVvi5ONp6lwgmIlWG72NKtZUB9pU&callback=initMap"
-        async defer></script>
+<?php 
+  $address1 = $_POST['address1']; 
+  $address2 = $_POST['address2'];
+?> 
 
-    <!-- geolocation -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.0.0/jquery.js"></script>
+
+<!----------------------------FORMAT ADDRESS---------------------------------------->  
+
+<script>
+
+function FormatAddress(arr, var_lat, var_long, myLatLng, i) 
+            {
+                var_address = arr.formatted_address;
+                console.log(var_address);
+                var_lat = [parseFloat((arr.geometry.location.lat))];
+                var_long = [parseFloat((arr.geometry.location.lng))];
+                
+                myLatLng = [var_lat,var_long];
+                
+                $("#geocode").append(var_address+"<br>");
+                return myLatLng;
+            }
+</script>
+
+<!-------------------------GET ADDRESS INPUTS--------------------------------------->
+
+<script>
+  
+var addresses = ["<?php echo $address1?>","<?php echo $address2?>"];
+var url = ["https://maps.googleapis.com/maps/api/geocode/json?address="+addresses[0]+"&key=AIzaSyBN5Q_GVvi5ONp6lwgmIlWG72NKtZUB9pU","https://maps.googleapis.com/maps/api/geocode/json?address="+addresses[1]+"&key=AIzaSyBN5Q_GVvi5ONp6lwgmIlWG72NKtZUB9pU"];
+var myArr = [];
+  
+</script>  
+
+<!------------------------RECEIVE FORMATTED ADDRESS-------------------------------->
+
+<script>
+
+var var_lat = [];
+var var_long = [];
+var var_address = [];  
+var myLatLng = [];
+for (i = 0; i <= 1; i++)
+{
+  console.log(i);
+  $.ajax
+  ({
+    dataType: 'json',
+    url: url[i],
+    success: function(response)
+    {
+      var_address[i] = response.results[0].formatted_address;
+      var_lat[i] = [parseFloat(response.results[0].geometry.location.lat)];
+      var_long[i] = [parseFloat(response.results[0].geometry.location.lng)];
+      console.log(var_address[i], var_lat[i], var_long[i]);
+      $("#geocode").append(var_address[i]+"<br>");
+    }
+  })
+}
+
+
+</script>
+  
+<!-------------------------------CREATE MAP------------------------------------------>  
+  
+<script>
+var map;
+function initMap(myLatLng, response) 
+  {
+   myLatLng = {lat: 52, lng:-1.5};
+   map = new google.maps.Map(document.getElementById('map'),{center: {lat: 52, lng: -1.5},zoom: 5})
     
+   var marker = new google.maps.Marker({position: myLatLng, map: map, title: "address"});
+    
+   //service = new google.maps.places.PlacesService(map);
+   //service.textSearch(request, callback);
+   //var request = {location: pyrmont,radius: '500',query: 'restaurant'}
+  }
+  
+</script>
+
+<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBN5Q_GVvi5ONp6lwgmIlWG72NKtZUB9pU&callback=initMap"async defer></script>
+  
+<!----------------------------------PLACES LIBRARY--------------------------------------->
+  
+<script type="text/javascript" src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBN5Q_GVvi5ONp6lwgmIlWG72NKtZUB9pU&libraries=places"></script> 
+  
+<!--------------------------------------------------------------------------------------->
+  
+
+  
+
+
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
 </body>
 </html>
-
-<!--AIzaSyBN5Q_GVvi5ONp6lwgmIlWG72NKtZUB9pU-->
